0d4d22cd0d655c80edd10c91754492f8d69e5589
fdy <1124413480@qq.com>
2018-12-10 19:50:05 +0800

    gonglue

--- taotao/app/urls.py
+++ taotao/app/urls.py
@@ -1,5 +1,6 @@
 from flask_restful import Api
 
+from app.api.stategy import CategoryModel
 from app.api.userm import UserModel
 
 api = Api()
@@ -7,3 +8,6 @@ def init_urls(app):
     api.init_app(app)
 
 api.add_resource(UserModel,'/user/')
+
+# 攻略
+api.add_resource(CategoryModel,'/category/')
