6e89956539f8fc038c48b8fcbfe3dc9d39030ff7
whq <1015858214@qq.com>
2018-12-12 15:46:12 +0800

    whq

--- taotao/manage.py
+++ taotao/manage.py
@@ -1,11 +1,11 @@
-from flask_cors import CORS
+
 from flask_migrate import MigrateCommand
 from flask_script import Manager
 
 from app import create_app
 
 app = create_app('developConfig')
-CORS(app, resources=r'/*')
+
 manage = Manager(app)
 manage.add_command('db',MigrateCommand)
 if __name__ == '__main__':
